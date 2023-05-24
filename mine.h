1f7d6f03731b508841b4b468d25c7a1be3b6b250
trust447 <isaactrust447@gmail.com>
2023-05-24 03:49:54 -0700

    go

---
+++ mine.h
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+size_t reader(void *ptr, size_t size, size_t count, FILE *stream)
+{
+	size_t read;
+	 read = fread(ptr, size, count, stream);
+	 return (read);
+}	
+
+
+char *my_strncpy(char *dest, const char *src, size_t n)
+{
+	 char *result = strncpy(dest, src, n);
+	 return (result);
+
+}
+
+
+int stderror(const char *format, ...)
+{
+	int error;
+	va_list args;
+	va_start(args, format);
+	error = fprintf(stderr, format, args);
+	va_end(args);
+	return (error);
+
+}
+
+
+int my_fprintf(FILE *stream, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int result = vfprintf(stream, format, args);
+	va_end(args);
+	return result;
+}
+
