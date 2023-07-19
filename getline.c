d6d4732ace2516126c0279accc01db4d4b982442
BenSanti <bensonchineduotti@gmail.com>
2023-07-19 04:50:53 -0700

    remove write function from function

--- getline.c
+++ getline.c
@@ -8,9 +8,9 @@
 char *get_cmd(void)
 {
 	char *buf = NULL;
+	char *str_buf;
 	size_t size = 0;
 	ssize_t n;
-	char *str_buf;
 	size_t buf_len;
 
 	n = getline(&buf, &size, stdin);
@@ -21,6 +21,6 @@ char *get_cmd(void)
 	}
 	str_buf = buf;
 	buf_len = _strlen(str_buf);
-	write(1, buf, buf_len);
+
 	return (buf);
 }
